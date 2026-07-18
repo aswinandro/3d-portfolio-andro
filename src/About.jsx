fd332609f6a3e561130129b4cfb5a5aff7831648
Aswin Andro <aswin.knight@gmail.com>
2026-07-18 20:40:49 +0400

    fix

--- src/About.jsx
+++ src/About.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Experience from './sections/Experience';
-import TechStack from './sections/TechStack';
 
 const AboutPage = () => {
   return (
     <>
       <Experience />
-      <TechStack />
     </>
   );
 };
